3908d5cb653836c14dcbb6b0c31334a49f93afc3
fabricio21777 <fabriciosouza21777@gmail.com>
2023-06-20 09:02:29 -0300

    initial form

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,9 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElInput: typeof import('element-plus/es')['ElInput']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { reactive, ref } from 'vue'
+const labelPosition = ref('right')
+const form = reactive({
+  restaurantName: '',
+
+})
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+	<h2>Hello</h2>
+
+	<el-form
+		:model="form"
+		label-width="120px"
+		:label-position="labelPosition"
+		>
+		<el-form-item label="Nome do restaurante">
+			<el-input v-model="form.restaurantName"></el-input>
+
+		</el-form-item>
+	</el-form>
   </main>
 </template>
